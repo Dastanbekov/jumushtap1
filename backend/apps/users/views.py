4d26be429c742fc8734d6539a93a9df0bcabc214
Dastanbekov <dastanbekovaitmyrza@gmail.com>
2026-01-30 23:58:04 +0600

    feat: new api view, users/me

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,9 +1,8 @@
-# apps/users/views.py
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import UserRegistrationSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView, APIView
+from .serializers import UserRegistrationSerializer, WorkerProfileSerializer,BusinessProfileSerializer,IndividualProfileSerializer
 
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
@@ -24,3 +23,33 @@ class UserRegistrationView(generics.CreateAPIView):
 class CustomTokenObtainPairView(TokenObtainPairView):
     # Здесь можно добавить кастомный сериалайзер, если нужно возвращать еще и роль юзера вместе с токеном
     pass
+
+class UserMeView(APIView):
+    """
+    Возвращает данные текущего залогиненного пользователя
+    и его профиля в зависимости от роли.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        data = {
+            "id": user.id,
+            "email": user.email,
+            "phone": str(user.phone),
+            "user_type": user.user_type,
+        }
+
+        # Подмешиваем данные профиля
+        try:
+            if user.user_type == 'worker':
+                data.update(WorkerProfileSerializer(user.worker_profile).data)
+            elif user.user_type == 'business':
+                data.update(BusinessProfileSerializer(user.business_profile).data)
+            elif user.user_type == 'individual':
+                data.update(IndividualProfileSerializer(user.individual_profile).data)
+        except Exception as e:
+            # На случай, если юзер есть, а профиль почему-то не создался (баг)
+            data['profile_error'] = "Profile not found"
+
+        return Response(data)
